0c3ec0b6589c628a159f203699508d51e9fd27f0
Matthew Rada <matthewr7.christopher@gmail.com>
2022-10-04 18:53:32 -0400

    recursive assay

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Batch, Note
+from .models import Assay, Batch, Note
 admin.site.register(Batch)
 admin.site.register(Note)
+admin.site.register(Assay)
--- backend/base/models.py
+++ backend/base/models.py
@@ -1,12 +1,26 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 
 # Create your models here.
 class Note(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
   body = models.TextField()
+
+
+class Assay(models.Model):
+  name = models.CharField(max_length=25, null=True)
+  code = models.CharField(max_length=25, null=True)
+  group = models.ManyToManyField('self', blank=True)
+  #if assay contains a group do not include in group list
+  def __str__(self):
+    return f'{self.code}-{self.name}'
+
 
 def validate_nonzero(value):
     if value == 0:
@@ -16,34 +30,30 @@ def validate_nonzero(value):
         )
 
 class Batch(models.Model):
-  # firstAccessionNumber = models.CharField(max_length=25)
-  # lastAccessionNumber = models.CharField(max_length=25)
-  # client = models.CharField(max_length=25)
+  groupAssay = models.ForeignKey(Assay, null=True, blank=True, on_delete=models.SET_NULL)
 
-  assayCode = models.CharField(max_length=25)
+  # assayCode = GenericForeignKey(singleAssay, groupAssay)
   #assay name associated with assay code
 
-  # numberOfSamples = models.PositiveSmallIntegerField(default=0, validators=[validate_nonzero])
-  # #number of tests in assay code
-  # #calculate number of tests = tests in assay code * number of samples
+  numberOfSamples = models.PositiveSmallIntegerField(default=0, validators=[validate_nonzero])
+  #number of tests in assay code
+  #calculate number of tests = tests in assay code * number of samples
 
-  # isBatchProccessed = models.BooleanField(default=False)
+  isBatchProccessed = models.BooleanField(default=False)
 
-  batchDate = models.DateTimeField(auto_now_add=True)
+  batchDate = models.DateTimeField(default=datetime.now)
   # # dueDate = batchDate + 48hours
 
-  # TYPES = (
-  #   ('dna', 'DNA'),
-  #   ('rna', 'RNA'),
-  #   ('dna&rna', 'DNA & RNA')
-  # )
+  miscFields = models.JSONField(blank=True, default=dict)
+  # geneticType
+  # firstAccessionNumber
+  # lastAccessionNumber
+  # client
+  # group id's
 
-  # geneticType = models.CharField(choices=TYPES)
 
 
-# class Assay(models.Model):
-#   name = models.CharField(max_length=25)
-#   code = models.CharField(max_length=25)
+
 
 
 
