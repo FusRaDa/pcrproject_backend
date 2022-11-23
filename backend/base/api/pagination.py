84ad9932ed49343d845312b98aedb88ccf5f2613
Matthew Rada <matthewr7.christopher@gmail.com>
2022-11-23 10:21:30 -0500

    basic pagination

--- backend/base/api/pagination.py
+++ backend/base/api/pagination.py
@@ -2,9 +2,9 @@ from rest_framework import pagination
 from rest_framework.response import Response
 
 class BatchPagination(pagination.PageNumberPagination):
-  page_size = 100
+  page_size = 10
   page_size_query_param = 'page_size'
-  max_page_size = 1000
+  max_page_size = 10
 
   def get_paginated_response(self, data):
     return Response({
